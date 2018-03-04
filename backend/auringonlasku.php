c99aedd9057272b420bffcba74a24c2e35e74bcb
Sara Heljasvuo <sara.heljasvuo@iki.fi>
2018-03-04 09:01:25 +0200

    first commit

---
+++ backend/auringonlasku.php
@@ -0,0 +1,11 @@
+<?php
+try{	
+	$db = new PDO('sqlite:Auringonlasku.sqlite');	
+	$sql  = "SELECT Aika, Paikka, Lat, Lng, Arvio, Kuvaus FROM Auringonlaskut ORDER BY Aika DESC";
+	$array = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);	
+	echo json_encode($array);
+}catch(PDOException $e){
+	echo("Ei toimi!" . $e);
+}
+$db=null;
+?> 
